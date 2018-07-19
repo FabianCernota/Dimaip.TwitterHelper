5c17492648147bd0d2a51bfe8de0179b9f677ab3
fabs <fabs@uplink.world>
2018-07-19 13:20:02 +0200

    Update settings

--- Classes/FabianCernota/TwitterHelper/Eel/TwitterHelper.php
+++ Classes/FabianCernota/TwitterHelper/Eel/TwitterHelper.php
@@ -4,20 +4,19 @@ namespace FabianCernota\TwitterHelper\Eel;
 use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Configuration\ConfigurationManager;
 
 /**
  * Eel helper as a wrapper around Twitter API
  */
 class TwitterHelper implements ProtectedContextAwareInterface {
 	/**
-	 * Inject the settings
-	 *
-	 * @param array $settings
-	 * @return void
-	 */
-	public function injectSettings(array $settings) {
-			$this->settings = $settings;
-	}
+	* @Flow\InjectConfiguration(package="FabianCernota.TwitterHelper")
+	* @var array
+	*/
+	protected $settings = array();
+
+
 
 	/**
 	 * @param string $requestType
@@ -25,16 +24,21 @@ class TwitterHelper implements ProtectedContextAwareInterface {
 	 * @return array
 	 */
 	public function getRequest(string $requestType, string $get) {
+
 		$settings = array(
 			'oauth_access_token' => $this->settings['oauthAccessToken'],
 			'oauth_access_token_secret' => $this->settings['oauthAccessTokenSecret'],
 			'consumer_key' => $this->settings['consumerKey'],
 			'consumer_secret' => $this->settings['consumerSecret']
 		);
+
+
 		$twitter = new \TwitterAPIExchange($settings);
 		$response = $twitter->setGetfield('?' . $get)
 			->buildOauth('https://api.twitter.com/1.1/' . $requestType . '.json', 'GET')
 			->performRequest();
+
+
 		return json_decode($response);
 	}
 
